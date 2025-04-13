21f3da71add09c15335043d75ed47d05937c0966
DamnedChicken <144023538+DamnedChicken@users.noreply.github.com>
2025-04-14 00:17:33 +0530

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,12 +1,17 @@
-/* initializw thw count as zero
-cliicks gets the increment
+/* initialize thw count as zero
+clicks gets the increment
 increment the count variable when the button is clicked
 */
 
 let countEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el");
 let count = 0;
 function increment() {
   count += 1;
   countEl.innerText = count;
+}
+function save() {
+  let saveEl = document.getElementById("save-el");
+  let saveEnt = count + "-";
+  saveEl.innerText += saveEnt;
+  console.log(count);
 }
